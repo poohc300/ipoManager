eaa43a031d57204d097166270566a1d828b73794
poohc300 <poohc300@gmail.com>
2023-07-05 00:16:41 +0900

    puppeteer 로직 함수로 분리, thinkpool 시장특징주 이슈종목 테이블 조회

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ipoManager
 공모주 정보 크롤러
+
+주요 사이트: 
+
+팍스넷 https://www.paxnet.co.kr/
+
+씽크풀 https://www.thinkpool.com/
+
+오픈다트(공시관련서비스) https://opendart.fss.or.kr/intro/infoApiList.do
+
+// 키워드 잘 추려서 유상증자 들어가면 파란색 자사주 소각하면 빨간색
--- index.js
+++ index.js
@@ -2,22 +2,60 @@ const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
 
-app.get('/', async(req, res) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+app.use(function(req, res, next) {
+    res.setHeader("Content-Type", "application/json");
+    next();
+});
 
-    const baseUrl = 'http://www.38.co.kr/html/fund/index.htm?o=k&page=1';
-    await page.goto(baseUrl);
+app.use(express.json());
+ 
+// express에는 json 데이터를 파싱하는 모듈이 내장되어있다.
+// 하지만 json만 되고 x-www-form-urlencoded를 파싱하기 위해서 아래를 확장해야 한다.
+app.use(express.urlencoded({
+  extended: true
+}))
+ 
+const callPage = async (url, element) => {
+    const browser = await puppeteer.launch({
+        headless : false
+      });
+    const page = await browser.newPage();
+    await page.goto(url);
 
     const content = await page.$eval(
-        `body > table:nth-child(9) > tbody > tr > td > table:nth-child(2) > tbody > tr > td:nth-child(1) > table:nth-child(11) > tbody > tr:nth-child(2) > td > table > tbody > tr`,
-        (el) => el.textContent
-    );
+        element,
+        (el) => {
+            return el.textContent;
+        }
+    )
+
     await page.close();
     await browser.close();
-    res.send(content);
+
+    return content;
+}
+
+app.get('/ipoSchedule', async(req, res) => {
+    // 공모주 청약 일정
+   
+    const baseUrl = 'http://www.38.co.kr/html/fund/index.htm?o=k&page=1';
+    const element = 'body > table:nth-child(9) > tbody > tr > td > table:nth-child(2) > tbody > tr > td:nth-child(1) > table:nth-child(11) > tbody > tr:nth-child(2) > td > table > tbody > tr';
+    const element2 = 'body > table:nth-child(9) > tbody > tr > td > table:nth-child(2) > tbody > tr > td:nth-child(1) > table:nth-child(11) > tbody > tr:nth-child(2) > td > table > tbody';
+
+    let result = await callPage(baseUrl, element2)
+    console.log(result);
+    res.json(result);
 
 });
+
+app.get('/thinkpool/marketFeatureNote', async(req, res) => {
+    const url = 'https://www.thinkpool.com/';
+    const element = '#content > div:nth-child(6) > div > div > div.main-items-tab > div:nth-child(2) > div.tab-pane.in.active > div.table_w.mt-20 > table';
+
+    let result = await callPage(url, element);
+    console.log(result);
+    res.json(result);
+})
 
 app.listen(3000, () => {
     console.log("server listening on 3000");
